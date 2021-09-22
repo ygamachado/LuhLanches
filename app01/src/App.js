07a2c9248acc7a3a749b57988015ea62482b8b77
ymcevddesk <ygaelm@gmail.com>
2021-09-22 17:09:09 -0300

    ss

--- app01/src/App.js
+++ app01/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          App Lanchonete
+          App Lanchonetes
         </a>
       </header>
     </div>
