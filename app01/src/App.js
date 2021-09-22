ab7a721c26247625e759848ed266bbaf150fa188
ymcevddesk <ygaelm@gmail.com>
2021-09-22 15:54:18 -0300

    first commit..

--- app01/src/App.js
+++ app01/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacts
+          Learn React
         </a>
       </header>
     </div>
